550018121957d11540b61f3b2ffc28a3f440c09f
liuqianshun <qianshunliu_1994@163.com>
2017-12-15 13:46:28 +0800

    SpringBoot类型安全配置

---
+++ springbootsecuritybean/src/test/java/com/liuqs/springbootsecuritybean/SpringbootsecuritybeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liuqs.springbootsecuritybean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootsecuritybeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
